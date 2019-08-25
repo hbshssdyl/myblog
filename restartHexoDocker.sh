d25e06ce4bc2337f67aca17adc96b6a405a06619
Lyself <lyself@LyselfdeMacBook-Pro.local>
2019-08-25 16:57:07 +0800

    update_theme

---
+++ restartHexoDocker.sh
@@ -0,0 +1,4 @@
+docker stop myBlog
+docker rm myBlog
+# docker run --name myBlog -v "$(pwd):/myBlog" -d -p 4000:4000 myblog
+docker run --name myBlog -v "$(pwd)":/myBlog -d -p 80:4000 ccr.ccs.tencentyun.com/dylandocker/myblog:19.08
